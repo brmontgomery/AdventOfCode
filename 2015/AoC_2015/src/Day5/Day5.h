fd0a8812c0bd25dee9e2b74cef98f18bacba0a35
brmontgomery <brian@montgomerycrew.net>
2024-05-16 09:05:10 -0700

    Day 5 Part 1 Complete

--- 2015/AoC_2015/src/Day5/Day5.h
+++ 2015/AoC_2015/src/Day5/Day5.h
@@ -9,25 +9,25 @@ void AoC2015D5P1() {
     std::string vowels = "aeiou";
     
     for (int i = 0; i < input.size(); i++) {
-        if (input[i].find("ab") != std::string::npos && input[i].find("cd") != std::string::npos && input[i].find("pq") != std::string::npos && input[i].find("xy") != std::string::npos) {
-            char lastChar = '';
+        if (input[i].find("ab") == std::string::npos && input[i].find("cd") == std::string::npos && input[i].find("pq") == std::string::npos && input[i].find("xy") == std::string::npos) {
+            char lastChar = ' ';
             int vowelCount = 0;
             bool doubleFlag = false;
             
-            for (int j = 0; j < input[i].size() && (vowelCount < 3 || doubleFlag == false); i++) {
+            for (int j = 0; j < input[i].size() && (vowelCount < 3 || doubleFlag == false); j++) {
                 if (vowelCount < 3) {
                     if (vowels.find(input[i][j]) != std::string::npos) {
                         vowelCount++;
                     }
                 }
-                if (doubleFlag = false) {
+                if (doubleFlag == false) {
                     if (input[i][j] == lastChar) {
                         doubleFlag = true;
                     }
                 }
                 lastChar = input[i][j];
             }
-            if (vowelCount >= 3 && doubleFlag = true) {
+            if (vowelCount >= 3 && doubleFlag == true) {
                 niceCount++;
             }
         }
--- 2015/AoC_2015/src/main.cpp
+++ 2015/AoC_2015/src/main.cpp
@@ -4,8 +4,9 @@
 #include "Day2/Day2.h"
 #include "Day3/Day3.h"
 #include "Day4/Day4.h"
+#include "Day5/Day5.h"
 
 int main() {
-	AoC2015D4P2();
+	AoC2015D5P1();
 	system("PAUSE");
 }
