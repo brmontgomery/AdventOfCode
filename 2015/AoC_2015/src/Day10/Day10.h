e3765215335769a7f0ac8eac59aca7cd93464711
brmontgomery <32968445+brmontgomery@users.noreply.github.com>
2024-05-20 13:55:47 -0700

    Start of Day 10 Attempt

--- 2015/AoC_2015/src/Day10/Day10.h
+++ 2015/AoC_2015/src/Day10/Day10.h
@@ -4,7 +4,29 @@
 
 void AoC2015D10P1() {
     std::vector<std::string> input = getFileInput(".//src//Day10//Day10.txt");
-    
+
+    int count = 0, charCount = 0;
+    std::string result;
+    while (count < 40) {
+        result = "";
+        charCount = 0;
+        char lastChar = '';
+        for (int i = 0; i < input[0].size(); i++) {
+            if (lastChar == '') {
+                lastChar = input[0][i];
+                charCount += 1;
+            } else if (lastChar != input[0][i]) {
+                result += std::to_string(charCount) + lastChar;
+                charCount = 1;
+                lastChar = input[0][i];
+            } else {
+                charCount += 1;
+            }
+            result += std::to_string(charCount) + lastChar;
+            input[i] = result;
+        }
+    }
+    std::cout << input[i] << std::endl << std::endl;
 }
 
 void AoC2015D10P2() {
