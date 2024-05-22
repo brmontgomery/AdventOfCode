8a0169bb07649dac8045c9a3be88eb87b2389bb4
brmontgomery <32968445+brmontgomery@users.noreply.github.com>
2024-05-22 13:12:23 -0700

    Update and rename Day3.h to Day13.h

---
+++ 2015/AoC_2015/src/Day13/Day13.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include "AoCUtilities.h"
+
+void AoC2015D13P1() {
+    std::vector<std::string> input = getFileInput(".//src//Day13//Day13.txt");
+    
+}
+
+void AoC2015D13P2() {
+    std::vector<std::string> input = getFileInput(".//src//Day13//Day13.txt");
+    
+}
