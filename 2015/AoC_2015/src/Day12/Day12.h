399fd46d03d0694c09d6d31996fdef8db14b10b2
brmontgomery <32968445+brmontgomery@users.noreply.github.com>
2024-05-22 13:11:58 -0700

    First Attempt of Day 12

--- 2015/AoC_2015/src/Day12/Day12.h
+++ 2015/AoC_2015/src/Day12/Day12.h
@@ -4,7 +4,30 @@
 
 void AoC2015D12P1() {
     std::vector<std::string> input = getFileInput(".//src//Day12//Day12.txt");
+
+    int sum = 0;
     
+    for (int i = 0; i < input[0].size(); i++) {
+        if (input[0][i] == '-') {
+            int digits = 1;
+            std::string convert = "-";
+            while (isNumber(input[0][i + digits])) {
+                convert += input[0][i + digits];
+                digits++;
+            }
+            sum += std::stoi(convert);
+        } else if (input[0][i].isNumeric()) {
+            int digits = 0;
+            std::string convert = "";
+            while (isNumber(input[0][i + digits])) {
+                convert += input[0][i + digits];
+                digits++;
+            }
+            sum += std::stoi(convert);
+        }
+    }
+
+    std::cout << sum << std::endl << std::endl;
 }
 
 void AoC2015D12P2() {
