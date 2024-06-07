1bec4feb9e76e7f610fbb343e522ba9a95e0d572
brmontgomery <32968445+brmontgomery@users.noreply.github.com>
2024-06-07 16:25:54 -0700

    Update Day20.h

--- 2015/AoC_2015/src/Day20/Day20.h
+++ 2015/AoC_2015/src/Day20/Day20.h
@@ -1,10 +1,26 @@
 #pragma once
 #include <iostream>
+#include <cmath>
 #include "AoCUtilities.h"
 
 void AoC2015D20P1() {
     std::vector<std::string> input = getFileInput(".//src//Day20//Day20.txt");
-    
+    if (input[0] == 1) { std::cout << "1" << std::endl << std::endl; return; }
+
+    bool found = false;
+    int i = 2;
+    while (!found) {
+        int count  = 1;
+        for (int j = 2; j < sqrt(i); j++) {
+            if (i % j == 0) {
+                count += j;
+            }
+        }
+        if ((count + i) * 10 == input[0]) {
+            std::cout << std::stoi((count + i) * 10) << std::endl << std::endl;
+            found = true;
+        }
+    }
 }
 
 void AoC2015D20P2() {
