ae7afbb7234db5990d555bfebd24e56d15f3cd6e
brmontgomery <32968445+brmontgomery@users.noreply.github.com>
2024-06-07 10:45:12 -0700

    Update Day17.h Problem 1 Draft

--- 2015/AoC_2015/src/Day17/Day17.h
+++ 2015/AoC_2015/src/Day17/Day17.h
@@ -4,7 +4,24 @@
 
 void AoC2015D17P1() {
     std::vector<std::string> input = getFileInput(".//src//Day17//Day17.txt");
-    
+
+    int count = 0;
+    //since all combinations is equivalent to 2 ^ n (number of items), we can represent the combination list as an index from 0 to (2^20)-1
+    for (int i = 0; i < 2 ^ input.size(); i++) {
+        int sum = 0;
+        //for each index add up the values of items corresponding to positive bits
+        for (int j = 0; j < input.size(); j++) {
+            if ((j >> j) & 1) {
+                sum += input[j];
+            }
+        }
+        //add to count if they equal 150
+        if (sum == 150) {
+            count++;
+        }
+    }
+
+    std::cout << count << std::endl<< std::endl;
 }
 
 void AoC2015D17P2() {
